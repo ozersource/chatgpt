68a6a8b7e3ed2e20ac8b47625da686ad33d87514
Ozer <82645186+ozersource@users.noreply.github.com>
2023-10-26 21:54:28 +0800

    Update Chimera.py

--- g4f/Provider/Providers/Chimera.py
+++ g4f/Provider/Providers/Chimera.py
@@ -100,7 +100,7 @@ def _create_completion(api_key: str, model: str, messages: list, stream: bool, *
             )
             
             for chunk in response:
-                yield chunk.choices[0].delta.get("content", "")
+                yield str(chunk.choices[0].delta.get("content", "")).decode('utf-8')
             print(response)    
         except openai.error.PermissionError as e:
             yield e.user_message
